e7913d6880fe421a85850ca8d1e933ac3a2189ea
pranay8873 <pranaypuvaati@gmail.com>
2026-03-22 19:40:07 +0530

    sub string methods

--- oops/strings/String2.java
+++ oops/strings/String2.java
@@ -4,5 +4,6 @@ public class String2 {
     public static void main(String[] args) {
         String Str="Hello World";
         System.out.println(Str.substring(2,6));
+        System.out.println(Str);
     }
 }
