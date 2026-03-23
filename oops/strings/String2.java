ace137b2952bbdbcf440e24f1f677cc8b098aeb2
pranay8873 <pranaypuvaati@gmail.com>
2026-03-23 18:24:33 +0530

    added concat method in strings

--- oops/strings/String2.java
+++ oops/strings/String2.java
@@ -5,5 +5,8 @@ public class String2 {
         String Str="Hello World";
         System.out.println(Str.substring(2,6));
         System.out.println(Str);
+        String Str2=Str.concat(" from concat method");
+        System.out.println("Str remains unchanged even after concat with Str2 : "+Str+" Str2= "+Str2);
+
     }
 }
