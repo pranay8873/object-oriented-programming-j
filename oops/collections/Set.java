ec5792afe1f972a619843e3deb43fa67b9f0e331
pranay8873 <pranaypuvaati@gmail.com>
2026-05-07 22:49:45 +0530

    implementing abstract methods

--- oops/collections/Set.java
+++ oops/collections/Set.java
@@ -1,4 +1,14 @@
 package com.java.oops.collections;
 
+import java.util.HashSet;
+
 public class Set {
+    public static void main(String[] args) {
+        HashSet set1 = new HashSet();
+        set1.add(20);
+        set1.add("pranay");
+        System.out.println(set1);
+        set1.add("samuel");
+        System.out.println(set1);
+    }
 }
